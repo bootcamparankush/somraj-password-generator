b30b96a773c6d505812a4034f07449ef9d73239b
Parankush Work <parankushwork@Parankushs-iMac.local>
2023-01-16 10:28:30 -0500

    checking in updated script.js after code clean-up

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 var passwordLength = 8;
 var passwordChoicearray = [];
 
-//declaring arrays for special chars, lower/uppercase letters and numbers
-var specialCharArray = ["!","@","#","$","%","^","&","*","(",")","[","]","|","?","/","","<",">","'","+","=","-",";",":","~"];
+//declaring arrays for lower/uppercase letters, numbers and special chars.
 var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
+var specialCharArray = ["!","@","#","$","%","^","&","*","(",")","[","]","|","?","/","","<",">","'","+","=","-",";",":","~"];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -15,12 +15,13 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
+
 function writePassword() {
   
-  var validPrompts = getPrompts ();
+  var validPrompts = getUserPrompts ();
   var passwordText = document.querySelector("#password");
 
-  if (validPrompts) {
+if (validPrompts) {
     var newPassword = generatePassword();
     passwordText.value = newPassword;
   } else {
@@ -44,36 +45,41 @@ function generatePassword () {
 
 //function to prompt user input(s) for four various combinations
 
-function getPrompts (){
+function getUserPrompts (){
   
   passwordChoicearray = [];
+  
   passwordLength = parseInt(prompt("How many characters of length you want your password to be. Choose between 8 to 128 characters")); //parseInt converts string into a number
+  // passwordLength = prompt("How many characters of length you want your password to be. Choose between 8 to 128 characters"); //parseInt converts string into a number
   
   //validate if the password is between 8 and 128 characters
-  if (isNaN(passwordLength) || passwordLength <= 8 || passwordLength >= 128) { 
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) { 
     alert ("Password length needs to be a number between 8 to 128 digits. Please re-enter and try again")
     return false;
   }
  
+console.log (getUserPrompts);
+
   //prompts for lower case letters
  if( confirm ("Do you want your password to consist of lower case letters?")) {
     passwordChoicearray = passwordChoicearray.concat(lowerCaseArray);
+   console.log (passwordChoicearray); 
   }
 //prompts for upper case letters 
   if( confirm ("Do you want your password to consist of  upper case letters?")) {
     passwordChoicearray = passwordChoicearray.concat(upperCaseArray);
+    console.log (passwordChoicearray);
   }
 //prompts for numbers
   if( confirm ("Do you want your to consist of numbers?")) {
     passwordChoicearray = passwordChoicearray.concat(numbersArray);
+    console.log (passwordChoicearray);
   }
-
 //prompts for special characters
   if( confirm ("Do you want your password to consist of  special characters ?")) {
     passwordChoicearray = passwordChoicearray.concat(specialCharArray);
   }
+  console.log (passwordChoicearray);
   return true;
 }
 
-// console.log (getPrompts);
-// console.log (passwordChoicearray);
