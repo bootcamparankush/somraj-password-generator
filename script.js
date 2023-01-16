f45f3dd26b314ff39c7f3c265c3088a1bb04ef5b
Parankush Work <parankushwork@Parankushs-iMac.local>
2023-01-16 10:34:39 -0500

    checking in updated script code to repo

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 var passwordLength = 8;
 var passwordChoicearray = [];
 
-//declaring arrays for lower/uppercase letters, numbers and special chars.
+//declaring arrays for lower/uppercase letters, numbers and special characters.
 var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbersArray =   ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharArray = ["!","@","#","$","%","^","&","*","(",")","[","]","|","?","/","","<",">","'","+","=","-",";",":","~"];
 
-// Assignment Code
+// Assignment Code - Started code
 var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
@@ -29,7 +29,8 @@ if (validPrompts) {
   }
 }
 
-//function to generate random password
+//function to generate random password based on userPrompts.
+
 function generatePassword () {
 
   var password = "";
