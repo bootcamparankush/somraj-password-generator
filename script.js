9c1e62342e14c6d677c96c7cc2e0917c23f6668d
Parankush Work <parankushwork@Parankushs-iMac.local>
2023-01-16 10:30:39 -0500

    adding updated script file to repo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var passwordChoicearray = [];
 //declaring arrays for lower/uppercase letters, numbers and special chars.
 var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
+var numbersArray =   ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharArray = ["!","@","#","$","%","^","&","*","(",")","[","]","|","?","/","","<",">","'","+","=","-",";",":","~"];
 
 // Assignment Code
